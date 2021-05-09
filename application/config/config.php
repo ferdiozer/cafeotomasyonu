52e455e8cfbb8b2b8ce47b235a450e34fa28d0bf
Ferdi Özer <info@ferdiozer.com>
2021-05-10 00:55:39 +0300

    fix:configuration

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://jupiter.bugsoft.net';
+$config['base_url'] = 'https://jupiter.piyanos.com';
 
 /*
 |--------------------------------------------------------------------------
