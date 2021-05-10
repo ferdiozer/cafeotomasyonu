d079f9ae21dbab67bc211d911b5e2ad552fffd31
Ferdi Özer <info@ferdiozer.com>
2021-05-10 16:41:16 +0300

    fix:version and copy

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <div class="pull-right"><b>CI</b> v<?=CI_VERSION?></div>
-    <div class="pull-right"><b>Version</b> Beta-v1.0.0</div>
+    <div class="pull-right"><b>Beta</b>v1.0.0  </div>
     <strong>
-        Copyright © <?php echo date('Y'); ?> |
+        © <?php echo date('Y'); ?> powered by
         <a title="Development" target="_blank" href="https://piyanos.com">Piyanos Software
         </a>
     </strong>
