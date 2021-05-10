1a43046f320b34f1399c2a9c7f3abcf0742fda99
Ferdi Özer <info@ferdiozer.com>
2021-05-10 15:26:38 +0300

    fix:footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,8 +1,9 @@
 <footer class="main-footer">
     <div class="pull-right"><b>Beta</b> 1.0.0</div>
+    <div class="pull-right"><b>CI</b> v<?=CI_VERSION?></div>
     <strong>
         Copyright © <?php echo date('Y'); ?> |
-        <a target="_blank" href="http://bugsoft.net">Bugsoft
+        <a title="Development" target="_blank" href="https://piyanos.com">Piyanos Software
         </a>
     </strong>
 </footer>
